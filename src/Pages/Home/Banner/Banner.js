298dd62df098616fc990cfbf80df53301bddd893
mehedi hasan <mehedicsebu@gmail.com>
2022-05-08 09:07:42 +0600

    banner added

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,11 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Carousel } from 'react-bootstrap';
+
+import banner1 from "../../../images/banner/f-2.jpg"
+import banner2 from "../../../images/banner/f-3.jpg"
+import banner3 from "../../../images/banner/f-7.jpg"
+
 
 const Banner = () => {
+    const [index, setIndex] = useState(0);
+    const handleSelect = (selectedIndex, e) => {
+        setIndex(selectedIndex);
+    };
     return (
         <div>
-            <h2>
-                banner
-            </h2>
+            <Carousel activeIndex={index} onSelect={handleSelect}>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={banner1}
+                        alt="First slide"
+                    />
+                    <Carousel.Caption>
+
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={banner2}
+                        alt="Second slide"
+                    />
+
+                    <Carousel.Caption>
+
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={banner3}
+                        alt="Third slide"
+                    />
+
+                    <Carousel.Caption>
+
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
         </div>
     );
 };
